00518a1fce475a24e85459ac12efa5c964896cac
HeiseMax <max.heise@gmx.de>
2025-11-04 16:04:15 +0100

    Added CelebA dataloader.

--- mylib/data.py
+++ mylib/data.py
@@ -1,4 +1,7 @@
 import numpy as np
+import torch
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
 
 def make_half_sphere(n_samples=1000, noise=0.1, random_state=None):
     """
@@ -33,3 +36,24 @@ def make_half_sphere(n_samples=1000, noise=0.1, random_state=None):
     labels = z
     
     return np.column_stack((x, y, z)), labels
+
+def load_CelebA(transform=[], split=0.8, seed=36):
+    """
+    Load the CelebA dataset.
+    """
+    data_path = 'data/CelebA/'
+    train_dataset = datasets.ImageFolder(
+        root=data_path,
+        transform = transforms.Compose([
+            transforms.ToTensor(),
+            *transform
+        ])
+    )
+
+    # create train test split
+    train_size = int(split * len(train_dataset))
+    test_size = len(train_dataset) - train_size
+    generator = torch.Generator().manual_seed(seed)
+    train_dataset, test_dataset = torch.utils.data.random_split(train_dataset, [train_size, test_size], generator=generator)
+
+    return train_dataset, test_dataset
