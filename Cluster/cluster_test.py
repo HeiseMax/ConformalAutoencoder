76560b3deb6b6de720ebf943fb05c89f30c9ffc3
HeiseMax <max.heise@gmx.de>
2025-11-18 16:51:49 +0100

    Updated test.

--- Cluster/cluster_test.py
+++ Cluster/cluster_test.py
@@ -7,42 +7,50 @@ import torch
 
 from networks import Encoder, Decoder
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-print("Using device:", device)
+def test():
 
-a = torch.randn(4, 3, 64, 64)
-a = a.to(device)
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print("Using device:", device)
 
-latent_dim = 512
-channels = 3
-encoder = Encoder(gap_ch=1, z_dim=latent_dim, base_ch=32).to(device)
-decoder = Decoder(gap_ch=1, z_dim=latent_dim, base_ch=32).to(device)
+    a = torch.randn(4, 3, 64, 64)
+    a = a.to(device)
 
-print(summary(encoder, input_size=(1, channels, 64, 64)))
+    latent_dim = 512
+    channels = 3
+    encoder = Encoder(gap_ch=1, z_dim=latent_dim, base_ch=32).to(device)
+    decoder = Decoder(gap_ch=1, z_dim=latent_dim, base_ch=32).to(device)
 
-
-from data import CelebA
-CelebA_dataset = CelebA(split='train') #, filter_categories=[(15, False), (20, True)]
+    print(summary(encoder, input_size=(1, channels, 64, 64)))
 
 
-print(len(CelebA_dataset))
+    from data import CelebA
+    CelebA_dataset = CelebA(split='train') #, filter_categories=[(15, False), (20, True)]
 
 
-from torch.utils.data import DataLoader
-
-dataloader = DataLoader(CelebA_dataset, batch_size=32, shuffle=False, num_workers=4)
+    print(len(CelebA_dataset))
 
 
-for batch in dataloader:
-    images, labels = batch
-    break
+    from torch.utils.data import DataLoader
+
+    dataloader = DataLoader(CelebA_dataset, batch_size=32, shuffle=False, num_workers=4)
 
 
-plt.imshow(images[1].permute(1, 2, 0))
-plt.show()
+    for batch in dataloader:
+        images, labels = batch
+        break
+
+
+    plt.imshow(images[1].permute(1, 2, 0))
+    plt.show()
 
 
 
-from data import parse_celeba_attr_file
+    from data import parse_celeba_attr_file
 
-filenames, classnames, labels = parse_celeba_attr_file('data/CelebA/list_attr_celeba.txt')
+    filenames, classnames, labels = parse_celeba_attr_file('data/CelebA/list_attr_celeba.txt')
+
+    print("done")
+
+
+if __name__ == "__main__":
+    test()
